636177ea6ab6009b81844376c8202b67fd2eea5b
futa4095 <kick20000@gmail.com>
2021-02-23 15:26:40 +0900

    root pathを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show', as: :user
 
+  root to: 'books#index'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
