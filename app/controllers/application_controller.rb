61e3c778b47e8191ba43461f4595c90877a64ed3
futa4095 <kick20000@gmail.com>
2021-02-23 10:55:39 +0900

    自身のアカウント編集を可能にする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     books_path
   end
 
   def after_sign_out_path_for(resource)
     new_user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :self_introduction])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
+  get 'users/:id', to: 'users#show', as: :user
 
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
