e9226367f43e9e89501372021554910e1e56edc6
futa4095 <kick20000@gmail.com>
2021-02-24 22:53:38 +0900

    letter openerでパスワード再設定メールを確認可能にする

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   root to: 'books#index'
   resources :books
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
