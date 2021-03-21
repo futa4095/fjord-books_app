5185b29d4f3d8875e0786df28c3dfe23576ab984
futa4095 <kick20000@gmail.com>
2021-03-21 13:52:09 +0900

    パスワード入力なしでプロフィール編集可能にする

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,4 +5,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
     hash[:uid] = User.create_unique_string if hash
     super
   end
+
+  def update_resource(resource, params)
+    if params[:password].blank?
+      params.delete(:current_password)
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
 end
