f41e571ba1a4125bef374505ca0c846f0044ad08
futa4095 <kick20000@gmail.com>
2021-03-28 19:54:29 +0900

    複数箇所のUser取得をまとめる

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
 
 class FollowersController < ApplicationController
+  before_action :set_user
+
   def index
-    @user = User.find(params[:user_id])
     @followers = @user.followers.order(:id).page(params[:page])
   end
 
   def create
-    @user = User.find(params[:user_id])
     @user.followers << current_user
     redirect_to @user
   end
 
   def destroy
-    @user = User.find(params[:user_id])
     @follow = Follow.find(params[:id])
     @follow.destroy
     redirect_to @user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
