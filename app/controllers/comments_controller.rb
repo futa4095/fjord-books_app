9dfc324fd265a2ff6f5950d2820d6d9ae6231b9c
futa4095 <kick20000@gmail.com>
2021-04-10 21:28:33 +0900

    URL取得にpolymorphic_pathヘルパーを使用しないようにする

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,9 +16,9 @@ class CommentsController < ApplicationController
     resource, id = request.path.split('/')[1, 2]
     @comment.commentable = resource.singularize.classify.constantize.find(id)
     if @comment.save
-      redirect_to polymorphic_path(@comment.commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+      redirect_to @comment.commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
-      redirect_to polymorphic_path(@comment.commentable), alert: @comment.errors.full_messages.join("\n")
+      redirect_to @comment.commentable, alert: @comment.errors.full_messages.join("\n")
     end
   end
 
