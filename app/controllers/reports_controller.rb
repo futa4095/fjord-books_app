759a7b9096b5a4894a44ac8bafa73ceba0ea0107
futa4095 <kick20000@gmail.com>
2021-04-11 13:40:11 +0900

    不要な自動生成コメント削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,26 +4,18 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
   before_action :deny_other_user, only: %i[edit update destroy]
 
-  # GET /reports
-  # GET /reports.json
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
-  # GET /reports/1
-  # GET /reports/1.json
   def show; end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit; end
 
-  # POST /reports
-  # POST /reports.json
   def create
     @report = Report.new(report_params)
 
@@ -34,8 +26,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reports/1
-  # PATCH/PUT /reports/1.json
   def update
     if @report.update(report_params)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
@@ -44,8 +34,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  # DELETE /reports/1
-  # DELETE /reports/1.json
   def destroy
     @report.destroy
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
