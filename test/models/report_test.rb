da4bf0cf19f943e691d539f096206d8b71567c05
futa4095 <kick20000@gmail.com>
2021-04-18 15:37:32 +0900

    User, Reportのモデルテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable' do
+    report = reports(:one)
+    alice = users(:alice)
+    assert report.editable?(alice)
+    bob = users(:bob)
+    assert_not report.editable?(bob)
+  end
+
+  test 'created_on' do
+    report = reports(:one)
+    assert_equal Date.new(2021, 4, 1), report.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name_or_email' do
+    alice = users(:alice)
+    assert_equal 'alice@example.com', alice.name_or_email
+    alice.name = 'alice'
+    assert_equal 'alice', alice.name_or_email
+  end
+
+  test 'follow and unfollow' do
+    alice = users(:alice)
+    bob = users(:bob)
+
+    assert_not alice.following?(bob)
+    assert_not bob.followed_by?(alice)
+    alice.follow(bob)
+    assert alice.following?(bob)
+    assert bob.followed_by?(alice)
+    alice.unfollow(bob)
+    assert_not alice.following?(bob)
+    assert_not bob.followed_by?(alice)
+  end
 end
